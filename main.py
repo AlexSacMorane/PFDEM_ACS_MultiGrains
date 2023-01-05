c277582b10623c9468146bbebb1e4da3a02e714c
AlexSacMorane <90468713+AlexSacMorane@users.noreply.github.com>
2023-01-05 10:46:02 +0100

    debug

--- main.py
+++ main.py
@@ -223,8 +223,8 @@ def iteration_main_until_pf(dict_algorithm, dict_material, dict_sample, dict_sol
     #look for the initial external energy sources
     if False:
         if 'Ed' in dict_algorithm['L_flag_plot']:
-        Owntools.PFtoDEM_Multi.Ed_PFtoDEM_Multi('Output/Ite_'+str(dict_algorithm['i_PFDEM'])+'/'+dict_algorithm['namefile']+'_'+str(dict_algorithm['i_PFDEM'])+'_other_000',dict_algorithm,dict_sample)
-        Owntools.Plot.Plot_Ed(dict_sample)
+            Owntools.PFtoDEM_Multi.Ed_PFtoDEM_Multi('Output/Ite_'+str(dict_algorithm['i_PFDEM'])+'/'+dict_algorithm['namefile']+'_'+str(dict_algorithm['i_PFDEM'])+'_other_000',dict_algorithm,dict_sample)
+            Owntools.Plot.Plot_Ed(dict_sample)
 
     #create i
     Owntools.Write.Write_i(dict_algorithm, dict_material, dict_sample, dict_sollicitation)
