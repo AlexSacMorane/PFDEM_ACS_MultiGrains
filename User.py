67c124c62cfc802560e6c0de2eb934507a017bed
AlexSacMorane <90468713+AlexSacMorane@users.noreply.github.com>
2023-01-17 11:12:31 +0100

    Increase factor_etai

--- User.py
+++ User.py
@@ -130,7 +130,7 @@ def All_parameters():
     Ed_level3 = 25
 
     #PF parameters
-    factor_etai = 1.5 #factor reltaed to the minimal distance between grains with same eta
+    factor_etai = 2.1 #factor reltaed to the minimal distance between grains with same eta
 
     #DEM parameters
     dt_DEM_crit = math.pi*min(L_R)/(0.16*nu+0.88)*math.sqrt(rho*(2+2*nu)/Y) #s critical time step from O'Sullivan 2011
